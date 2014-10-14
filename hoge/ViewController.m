4c4b290a591a979e89fff972eef9c5fe34d4e9da
rubyenumerable <ruby.enumerable@gamil.com>
2014-10-14 23:20:36 +0900

    Logの追加
    Good byを追加

--- hoge/ViewController.m
+++ hoge/ViewController.m
@@ -18,6 +18,10 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"Hello World");
+    NSLog(@"Hi");
+    NSLog(@"See you again");
+    NSLog(@"Good by");
 }
 
 - (void)didReceiveMemoryWarning
